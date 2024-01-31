2745eae0601245688fd69349d7c781e2f35b839a
Gyutae Kim <kiimwf0819@gmail.com>
2024-01-31 16:18:10 +0900

    섹션3-4. AppConfig 리팩터링
    - 중복 제거를 위한 리팩터링

--- src/main/java/springstudy/firstproject/AppConfig.java
+++ src/main/java/springstudy/firstproject/AppConfig.java
@@ -1,6 +1,7 @@
 package springstudy.firstproject;
 
 import springstudy.firstproject.policy.FixDiscountPolicy;
+import springstudy.firstproject.repository.MemberRepository;
 import springstudy.firstproject.repository.MemoryMemberRepository;
 import springstudy.firstproject.service.MemberService;
 import springstudy.firstproject.service.MemberServiceImpl;
@@ -11,11 +12,19 @@ public class AppConfig {
 
     // 역할에 대한 기능을 수행하는 구현체에 대한 설정을 해당 클래스에서 진행
 
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자 주입
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
