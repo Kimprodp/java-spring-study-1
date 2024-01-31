3085f7df71befc08ec99472e7ae61ef82b38804a
Gyutae Kim <kiimwf0819@gmail.com>
2024-01-31 22:01:27 +0900

    섹션4. 다양한 설정 형식 지원 - 자바 코드, XML
    - xml 기반의 스프링 빈 설정
    - appConfig.xml 추가 및 해당 파일로 스프링 빈 조회하는 테스트 코드 추가

---
+++ src/test/java/springstudy/firstproject/xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package springstudy.firstproject.xml;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import springstudy.firstproject.service.MemberService;
+
+class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
