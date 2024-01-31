63a14b24b147f6f68faa9d9fb1082e8b15a71067
Gyutae Kim <kiimwf0819@gmail.com>
2024-01-31 20:02:44 +0900

    섹션4. 스프링 빈 조회 - 기본
    - 스프링 컨테이너에 등록된 빈을 이름, 타입으로 조회했을 때 성공과 실패하는 테스트 추가

---
+++ src/test/java/springstudy/firstproject/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,46 @@
+package springstudy.firstproject.beanfind;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springstudy.firstproject.AppConfig;
+import springstudy.firstproject.service.MemberService;
+import springstudy.firstproject.service.MemberServiceImpl;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회") // 메서드 반환 타입이 아닌, 인스턴스의 타입으로 조회
+    void findBeanByType2() {
+        MemberService memberService = ac.getBean(MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class));
+    }
+}
