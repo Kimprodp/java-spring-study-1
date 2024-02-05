1227cfb14e3ff59803adcc0f331a5a685f41f30b
Gyutae Kim <kiimwf0819@gmail.com>
2024-02-05 21:36:27 +0900

    섹션9. 스코프와 Provider
    - 웹 소켓 스코프 테스트 시 Provider를 통해 빈 생성 시점을 지연 시킴

--- src/main/java/springstudy/firstproject/AppConfig.java
+++ src/main/java/springstudy/firstproject/AppConfig.java
@@ -11,29 +11,29 @@ import springstudy.firstproject.service.MemberServiceImpl;
 import springstudy.firstproject.service.OrderService;
 import springstudy.firstproject.service.OrderServiceImpl;
 
-@Configuration
+//@Configuration
 public class AppConfig {
 
     // 역할에 대한 기능을 수행하는 구현체에 대한 설정을 해당 클래스에서 진행
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
-    @Bean
-    public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
-        return new RateDiscountPolicy();
-    }
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberServiceImpl(memberRepository()); // 생성자 주입
-    }
-
-    @Bean
-    public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+//
+//    @Bean
+//    public DiscountPolicy discountPolicy() {
+////        return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
+//    }
+//
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(memberRepository()); // 생성자 주입
+//    }
+//
+//    @Bean
+//    public OrderService orderService() {
+//        return new OrderServiceImpl(memberRepository(), discountPolicy());
+//    }
 }
---
+++ src/main/java/springstudy/firstproject/common/MyLogger.java
@@ -0,0 +1,34 @@
+package springstudy.firstproject.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import java.util.UUID;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "request") // 이 빈은 HTTP 요청 당 하나씩 생성되고, HTTP 요청이 끝나는 시점에 소멸됨
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
